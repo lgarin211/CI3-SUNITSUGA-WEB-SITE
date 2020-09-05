e910e5bbe8de3922dd1175ed91b4e096d6a50c09
lgarin211 <lagustinus211@gmail.com>
2020-09-05 17:19:18 +0700

    run

---
+++ application/views/imglink.php
@@ -0,0 +1,2 @@
+<h1>gambar yang akan di pakai</h1>
+<img src="https://javastech.com/wp-content/uploads/2020/05/Teks-Editor-Untuk-Programmer.jpg" alt="">
---
+++ application/views/two.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <title>Dimension by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="icon" type="image/x-icon" href="<?= base_url('pac/one/')?>assets/img/favicon.ico" />
+    <link rel="stylesheet" href="<?=base_url('pac/html5up-dimension/')?>assets/css/main.css" />
+    <noscript>
+    <link rel="stylesheet" href="<?=base_url('pac/html5up-dimension/')?>assets/css/noscript.css" />
+</noscript>
+</head>
+<body class="is-preload">
+    <div id="wrapper">
+    <header id="header">
+    <div class="logo">
+    <i class="fas fa-laptop-code fa-2x">
+
+</i>
+</div>
+<div class="content">
+    <div class="inner">
+    <h1>Agustinus Pardamean Lumban Tobing</h1>
+    <p>hello semua welcome <strong>to website</strong> disini ada beberapa tutorial <strong>pemproraman</strong>yang sayang banget kalo di lewatkan</p>
+    <small>jangan lupa lihat CV saya di <a href="" >sini</a> ya.</small>
+</div>
+</div>
+<nav>
+    <ul>
+    <li>
+    <a href="index.html">Home</a>
+</li>
+<li>
+    <a href="<?=base_url('welcome/mycv');?>">My CV</a>
+</li>
+<li>
+    <a href="#more"> belum ada materi</a>
+</li>
+</ul>
+</nav>
+</header>
+<footer id="footer">
+    <p class="copyright">&copy; <a href="">lgrarin211</a>.</p>
+</footer>
+</div>
+<div id="bg">
+
+</div>
+<script src="<?=base_url('pac/html5up-dimension/')?>assets/js/jquery.min.js">
+</script>
+<script src="<?=base_url('pac/html5up-dimension/')?>assets/js/browser.min.js">
+</script>
+<script src="<?=base_url('pac/html5up-dimension/')?>assets/js/breakpoints.min.js">
+</script>
+<script src="<?=base_url('pac/html5up-dimension/')?>assets/js/util.js">
+</script>
+<script src="<?=base_url('pac/html5up-dimension/')?>assets/js/main.js">
+</script>
+</body>
+</html>
